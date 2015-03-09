c15279677d815cfebc6abe76188e0096141a1b3f
klokoSSPU <KLKJAKUB@seznam.cz>
2015-03-09 08:35:37 +0100

    Jakub Klokočník

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
-from app import app
-
-@app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Jakub'}
+    posts = [
+        { 
+            'author': {'nickname': 'Anonym'},
+            'body': 'Dnes je v Opave pekne!'
+        },
+        { 
+            'author': {'nickname': 'Pocasi'},
+            'body': 'You dont say'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
