0644e61b1fd5858d12eee8de0dd8c5356d5b858a
klokoSSPU <KLKJAKUB@seznam.cz>
2015-03-09 09:08:45 +0100

    Jakub Klokočník

--- app/views.py
+++ app/views.py
@@ -1,16 +1,36 @@
+from flask import render_template, flash, redirect
+from app import app
+from .forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
 def index():
     user = {'nickname': 'Jakub'}
     posts = [
-        { 
+        {
             'author': {'nickname': 'Anonym'},
-            'body': 'Dnes je v Opave pekne!'
+            'body': 'Dneska je v Opave pekne'
         },
-        { 
-            'author': {'nickname': 'Pocasi'},
+        {
+            'author': {'nickname': 'pocasi'},
             'body': 'You dont say'
         }
     ]
-    return render_template("index.html",
+    return render_template('index.html',
                            title='Home',
                            user=user,
                            posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Prihlaseni',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
